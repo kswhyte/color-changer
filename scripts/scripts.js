af5c740e11dda5dc1fbd3eacdc7cde95d2d6c14c
KINAN WHYTE <kinanwhyte@gmail.com>
2016-09-06 15:59:18 -0600

    adds color randomizer and finalizes functionality

---
+++ scripts/scripts.js
@@ -0,0 +1,17 @@
+
+$('#random-button').on('click', function() {
+  $('html').css('background-color', getColor());
+});
+
+function getColor(){
+  var ascii = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 65, 66, 67, 68, 69, 70];
+  var color = '#';
+  while(color.length < 7){
+    color += String.fromCharCode(ascii[Math.floor(Math.random() * 16)]);
+  }
+  return color;
+}
+
+$('#reset-button').on('click', function() {
+  $('html').css('background', 'white');
+});
