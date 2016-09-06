6af731bd6e7e68ba47a9078aea7a651dc45f77cf
KINAN WHYTE <kinanwhyte@gmail.com>
2016-09-06 16:32:09 -0600

    adds functionality with excluding white from random color generator

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,16 @@
 
 $('#random-button').on('click', function() {
-  $('html').css('background-color', getColor());
+  var randomColor = getColor();
+  while (randomColor === '#ffffff') {
+    getColor();
+  } 
+  $('html').css('background-color', randomColor);
 });
 
 function getColor(){
   var ascii = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 65, 66, 67, 68, 69, 70];
   var color = '#';
-  while(color.length < 7){
+  while (color.length < 7){
     color += String.fromCharCode(ascii[Math.floor(Math.random() * 16)]);
   }
   return color;
